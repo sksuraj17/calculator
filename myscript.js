d75bdd6dcc5501b2668799d888610babd8869c72
Suraj SK <surajsk2002@gmail.com>
2022-05-08 08:59:05 +0530

    Commit 3

---
+++ myscript.js
@@ -0,0 +1,14 @@
+
+let outputScreen = document.getElementById("output-screen");
+function display(num){
+    outputScreen.value+=num;
+}
+function Calculate(){
+    try{
+        outputScreen.value=eval(outputScreen.value)
+    }
+    catch(err)
+    {
+        alert("Invalid value")
+    }
+}
